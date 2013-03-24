ffef0e38623ea36c72cb7239d13c058cac97d4b1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-03-24 12:47:58 -0400

    sample add function with unit test

---
+++ src/add.js
@@ -0,0 +1,13 @@
+/**
+	Returns the sum of two numbers
+
+	@function add
+	@param a {Number}
+	@param b {Number}
+	@public
+*/
+function add(a, b) {
+	return a + b;
+}
+
+module.exports = add;
---
+++ test/add.js
@@ -0,0 +1,10 @@
+var add = require('../src/add');
+
+gt.module('add');
+
+gt.test('testing add', function () {
+	gt.func(add, 'add is a function');
+	gt.arity(add, 2, 'expects two arguments');
+	gt.equal(add(1, 4), 5, '1 + 4 = 5');
+	gt.equal(add(-1, 1), 0, '-1 + 1 = 0');
+});
