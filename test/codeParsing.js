8c3792eb44f3d80e8151a9fde60ab71274b43c72
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-04-05 22:37:34 -0400

    parsing names

--- test/codeParsing.js
+++ test/codeParsing.js
@@ -1,8 +1,50 @@
 gt.module('test code parsing');
 
+var check = require('check-types');
+
+function parseName(code) {
+	check.verifyString(code, 'missing code');
+	var reg = /(?:'|")(\s*\w+\s*)(?:'|")/;
+	var matched = reg.exec(code);
+	console.log(matched);
+	if (!Array.isArray(matched)) {
+		return null;
+	}
+	return matched[1];
+}
+
+function parseCode(code) {
+	check.verifyString(code, 'missing code');
+	var result = {};
+	var regex = /(?:gt|QUnit)\.test\('{\w}+'\)/;
+	var match = regex.exec(code);
+	result.name = match[0];
+	return result;
+}
+
+gt.test('single quote words', function () {
+	gt.equal(parseName("'foo'"), 'foo');
+	gt.equal(parseName("' foo'"), ' foo');
+	gt.equal(parseName("' foo\t'"), ' foo\t');
+});
+
+gt.test('double quote words', function () {
+	gt.equal(parseName('"foo"'), 'foo');
+	gt.equal(parseName('" foo"'), ' foo');
+	gt.equal(parseName('" foo\t "'), ' foo\t ');
+});
+
+/*
+gt.test('empty code', function () {
+	var code = "gt.test('empty', function () {});";
+	var parsed = parseCode(code);
+	gt.object(parsed, 'got parsed result');
+});
+
 gt.test('single assertion', function () {
 	var code = "gt.test('sample add usage', function () { \n\
 		gt.equal(add(2, 3), 5);\n\
 	});";
 	gt.string(code, 'have code');
 });
+*/
