a92ac8d24323881773db2610171475f9436821b6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-04-06 22:28:33 -0400

    setting up esprima

--- src/parserUnitTest.js
+++ src/parserUnitTest.js
@@ -1,7 +1,51 @@
 var check = require('check-types');
 
+function parseEqualArguments(equal) {
+	check.verifyString(equal, 'equal is not a string');
+
+	var reg2 = /([\W\w+]),\s*([\W\w+])/;
+	var reg3 = /([\W\w+]),\s*([\W\w+]),\s*([\W\w+])/;
+	if (reg3.test(equal)) {
+		var parts = reg3.exec(equal);
+		console.log('have 3 parts to equal', parts);
+		return {
+			op: parts[0],
+			expected: parts[1],
+			message: parts[2]
+		};
+	} else if (reg2.test(equal)) {
+		var parts = reg2.exec(equal);
+		console.log('have 2 parts to equal', parts);
+		return {
+			op: parts[0],
+			expected: parts[1]
+		};
+	}
+	console.log('could not match ANY equal arguments in', equal);
+	return null;
+}
+
+function parseEqual(line) {
+	var isEqualReg = /(?:gt|QUnit)\.equal\(([\W\w]+)\);/;
+	if (!isEqualReg.test(line)) {
+		return null;
+	}
+	var matches = isEqualReg.exec(line);
+	console.log('matches', matches);
+	var equalArguments = matches[1];
+	check.verifyString(equalArguments, 'invalid equal arguments');
+	var parsed = parseEqualArguments(equalArguments);
+	check.verifyObject(parsed, 'did not get parsed arguments');
+	return parsed.op + '; // ' + parsed.expected;
+}
+
 function parseAssertion(line) {
 	check.verifyString(line, 'missing line');
+
+	var equalParsed = parseEqual(line);
+	if (check.isString(equalParsed)) {
+		return equalParsed;
+	}
 
 	var equalReg = /(?:gt|QUnit)\.equal\(([\W\w]+),\s*([\W\w]+)\s*\)/;
 	var matched = equalReg.exec(line);
