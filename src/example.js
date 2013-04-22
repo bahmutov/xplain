e624e43a2df9bc095850d32b210731f8f4184d93
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-04-21 22:57:11 -0400

    example toggle name from unit test name

--- src/example.js
+++ src/example.js
@@ -7,18 +7,20 @@ function exampleDiv(name, apiExample) {
     check.verifyString(name, 'missing method name');
     check.verifyObject(apiExample, 'missing example code string');
 
+    var exampleName = getName(apiExample.code);
+    check.verifyString(exampleName, 'could not get example name');
+
     var id = name + '_example_' + ++exampleDivId + '_toggle';
-    var toggle = '<input class="toggle" type="button" value="example ' + exampleDivId + '" id="' + id + '">\n';
+    var toggle = '<input class="toggle" type="button" value="' + exampleName + '" id="' + id + '">\n';
     var o = '<div id="' + id + 'd" class="example namedCode">\n';
     var prettyCode = reformat(apiExample.code, true);
     check.verifyString(prettyCode, 'could not reformat\n', apiExample.code);
 
-    var exampleName = getName(apiExample.code);
-    check.verifyString(exampleName, 'could not get example name');
     o += '<span class="sampleName">' + exampleName + '</span>\n';
     o += '<pre class="prettyprint linenums">\n' + prettyCode + '</pre>\n';
     o += '</div>\n';
     return {
+        name: exampleName,
         toggle: toggle,
         code: o
     };
