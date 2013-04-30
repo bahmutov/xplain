079890e711d6fc250b986216e599ec86ede5ccb6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-04-30 17:24:34 -0400

    refactoring

---
+++ test/docsToModules.js
@@ -0,0 +1,11 @@
+gt.module('docs to modules');
+
+var d2m = require('../src/docsToModules');
+
+gt.test('basics', function () {
+    gt.arity(d2m, 1, 'expects single argument');
+});
+
+gt.test('basic example', function () {
+    
+});
