d6f56e168e0a3a28d882b5276c63591b33f2e762
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-05-02 22:20:15 -0400

    removed commented out code

--- src/html-generation/sample.js
+++ src/html-generation/sample.js
@@ -1,40 +1,15 @@
 var check = require('check-types');
-// var parseCode = require('../parser').parseCode;
-// var parseUnitTestCode = require('../parserUnitTest').parseUnitTestCode;
 var transform = require('../doc-transform/toHumanForm');
 var html = require('pithy');
-
-/*
-function sampleToCommentLike(testCode) {
-    check.verifyString(testCode, 'missing test code');
-    var parsed = parseCode(testCode);
-    check.verifyObject(parsed, 'could not parse\n' + testCode);
-    return parsed;
-}
-*/
 
 var sampleDivId = 1;
 function sampleDiv(name, apiExample) {
     check.verifyString(name, 'missing name');
     check.verifyObject(apiExample, 'missing documented');
     check.verifyObject(apiExample.comment, 'missing comment')
-    // console.dir(apiExample);
-    /*
-    var code = apiExample.comment.code;
-    check.verifyString(code, 'missing code for sample');
-    var parsed = sampleToCommentLike(code);
-    check.verifyObject(parsed, 'did not get sample from', code);
-    check.verifyString(parsed.name, 'there is no name for', code);
-    check.verifyString(parsed.code, 'there is no code for', code);
-    var humanForm = parseUnitTestCode(parsed.code);
-    // console.log('human form', humanForm);
-    if (!check.isString(humanForm)) {
-        console.log('could not convert', parsed.code, 'to human form');
-        humanForm = parsed.code;
-    }
-    check.verifyString(humanForm, 'could not convert to human form', parsed.code);
-    */
+
     var humanForm = transform(apiExample);
+    check.verifyString(humanForm, 'could not convert to human form');
 
     var sampleElement = html.div({
         id: sampleDivId++,
