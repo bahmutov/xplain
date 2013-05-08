c36e3acad8c267fae19ca537d33394438f6ae497
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-05-07 20:11:33 -0400

    removed a lot of commented out code

--- src/doc-transform/adapters/qunit/parsers.js
+++ src/doc-transform/adapters/qunit/parsers.js
@@ -4,30 +4,14 @@ var code = require('../../../utils/code');
 function parseEqualArguments(equal) {
     check.verifyString(equal, 'equal is not a string');
 
-    // console.log('splitting', equal);
     var split = code.split(equal);
     check.verifyArray(split, 'did not get array from', equal);
-    // console.log(split);
     var result = {
         op: split[0],
         expected: split[1]
     };
     return result;
 }
-
-/*
-function parseNumberArguments(args) {
-    check.verifyString(args, 'args is not a string');
-
-    // console.log('splitting', args);
-    var split = code.split(args);
-    check.verifyArray(split, 'did not get array from', args);
-    var result = {
-        op: split[0]
-    };
-    return result;
-}
-*/
 
 function parseOkArguments(args) {
     check.verifyString(args, 'args is not a string');
@@ -39,29 +23,6 @@ function parseOkArguments(args) {
     };
     return result;
 }
-
-/*
-function parseFuncArguments(args) {
-    check.verifyString(args, 'args is not a string');
-    var split = code.split(args);
-    check.verifyArray(split, 'did not get array from', args);
-    var result = {
-        op: split[0]
-    };
-    return result;
-}
-
-function parseArityArguments(args) {
-    check.verifyString(args, 'args is not a string');
-    var split = code.split(args);
-    check.verifyArray(split, 'did not get array from', args);
-    var result = {
-        op: split[0],
-        number: split[1]
-    };
-    return result;
-}
-*/
 
 // top level parsers for individual assertions
 function parseStrictEqual(line) {
@@ -83,30 +44,12 @@ function parseDeepEqual(line) {
         return null;
     }
     var matches = isEqualReg.exec(line);
-    // console.log('matches', matches);
     var equalArguments = matches[1];
     check.verifyString(equalArguments, 'invalid array equal arguments');
     var parsed = parseEqualArguments(equalArguments);
     check.verifyObject(parsed, 'did not get parsed arguments');
-    // console.log('array expression', parsed.op);
     return parsed.op + '; // ' + parsed.expected;
 }
-
-/*
-function parseNumber(line) {
-    var reg = /(?:gt|QUnit)\.number\(([\W\w]+)\);/;
-    if (!reg.test(line)) {
-        return null;
-    }
-    var matches = reg.exec(line);
-    // console.log('number matches', matches);
-    var args = matches[1];
-    check.verifyString(args, 'invalid number arguments');
-    var parsed = parseNumberArguments(args);
-    check.verifyObject(parsed, 'did not get parsed arguments');
-    return parsed.op + '; // returns a number';
-}
-*/
 
 function parseOk(line) {
     var reg = /(?:|QUnit\.)ok\(([\W\w]+)\);/;
@@ -122,34 +65,6 @@ function parseOk(line) {
     return parsed.op + '; // true';
 }
 
-/*
-function parseFunc(line) {
-    var reg = /(?:gt|QUnit)\.func\(([\W\w]+)\);/;
-    if (!reg.test(line)) {
-        return null;
-    }
-    var matches = reg.exec(line);
-    var args = matches[1];
-    check.verifyString(args, 'invalid number arguments');
-    var parsed = parseFuncArguments(args);
-    check.verifyObject(parsed, 'did not get parsed arguments');
-    return '// ' + parsed.op + ' is a function';
-}
-
-function parseArity(line) {
-    var reg = /(?:gt|QUnit)\.arity\(([\W\w]+)\);/;
-    if (!reg.test(line)) {
-        return null;
-    }
-    var matches = reg.exec(line);
-    var args = matches[1];
-    check.verifyString(args, 'invalid number arguments');
-    var parsed = parseArityArguments(args);
-    check.verifyObject(parsed, 'did not get parsed arguments');
-    return '// ' + parsed.op + ' is a function that expects '
-        + parsed.number + ' arguments';
-}
-*/
 var lineParsers = [
     parseStrictEqual, parseDeepEqual, parseOk
 ];
