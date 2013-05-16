e5ffc31882c5825328fad22d214f241f1e607dc2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-05-15 22:46:14 -0400

    adding tests for methods with $ sign

--- examples/qunit/add.js
+++ examples/qunit/add.js
@@ -5,6 +5,13 @@
 */
 function add(a, b) { return a + b; }
 
+/**
+    Function name starts with dollar sign
+
+    @method $sum
+*/
+function $sum(a, b) { return a + b; }
+
 /** @sample add */
 QUnit.test('adding numbers', function () {
     QUnit.strictEqual(add(2, 3), 5);
@@ -16,8 +23,23 @@ QUnit.test('QUnit assertions', function () {
 });
 
 /** @sample add */
+QUnit.test('$dollar sign', function () {
+    QUnit.deepEqual([1, 2], [1, 2]);
+});
+
+/** @sample add */
+QUnit.test('dollar sign at the end$', function () {
+    QUnit.deepEqual([1, 2], [1, 2]);
+});
+
+/** @sample add */
 QUnit.test('QUnit assertions test with multiple words', function () {
     QUnit.deepEqual([1, 2], [1, 2]);
     QUnit.equal('foo', 'foo');
     QUnit.ok(true, 'true is true');
+});
+
+/** @sample $sum */
+QUnit.test('sample for $sum', function () {
+    QUnit.equal($sum(2, 3), 5);
 });
