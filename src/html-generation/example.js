1eb7d4a12a88de8b1d5ab6bdf9ffa0117877dd73
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-05-16 21:48:34 -0400

    refactoring

--- src/html-generation/example.js
+++ src/html-generation/example.js
@@ -2,20 +2,11 @@ var check = require('check-types');
 var reformat = require('../utils/code').reformat;
 var transform = require('../doc-transform/toHumanForm');
 var html = require('pithy');
+var makeToggle = require('./toggle');
 
 var exampleDivId = 0;
 
-function makeToggle(id, label) {
-    check.verifyString(id, 'missing id');
-    check.verifyString(label, 'missing toggle label');
-    var toggleElement = html.input({
-        class: "toggle",
-        type: "button",
-        value: label,
-        id: id
-    });
-    return toggleElement;
-}
+
 
 function exampleDiv(name, apiExample, framework) {
     check.verifyString(name, 'missing method name');
