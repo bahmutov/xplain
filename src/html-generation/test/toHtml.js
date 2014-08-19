f0475aaa60c0a1c7a9d5355abf1d06f765da6ccc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-08-19 00:14:19 -0400

    printing generated folder

--- src/html-generation/test/toHtml.js
+++ src/html-generation/test/toHtml.js
@@ -6,6 +6,7 @@ var toHtml = require('../toHtml');
 var fs = require('fs');
 var fse = require('fs.extra');
 var path = require('path');
+var exec = require('child_process').exec;
 
 gt.async('html generation', function () {
   gt.func(toHtml, 'assumed toHtml is a function');
@@ -22,8 +23,15 @@ gt.async('html generation', function () {
   gt.ok(p, 'returned something');
   gt.func(p.then, 'returned thenable');
   p.then(function () {
-    var apiCssFilename = path.join(__dirname, '/out/api.css');
-    gt.ok(fs.existsSync(apiCssFilename), 'file ' + apiCssFilename + ' exists');
+    return q.nfcall(exec, 'ls -l out')
+      .then(function (output) {
+        console.log(output[0]); // stdout
+        var apiCssFilename = path.join(__dirname, '/out/api.css');
+        gt.ok(fs.existsSync(apiCssFilename), 'file ' + apiCssFilename + ' exists');
+      });
+  })
+  .then(function () {
     gt.start();
-  }).done();
+  })
+  .done();
 });
